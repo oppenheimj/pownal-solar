9b1ccca874e9dab5963f508bc04d0f362653cd60
Jonas Oppenheim <oppenheim10@gmail.com>
2020-03-07 20:05:00 -0500

    Add basic API and frontend

---
+++ api.py
@@ -0,0 +1,19 @@
+import flask
+from flask import render_template
+from flask import request
+
+import common
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/pownal-solar', methods=['POST'])
+def toggleRelay():
+    relayNum = request.args.get('relayNum')
+    common.toggleRelay(relayNum)
+    return ''
+app.run()
